cd2fdedb5ac698c81872be990cde883530ae4559
Hassan Ali <hassanali@appsol360.com>
2025-01-31 13:16:57 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,5 +2,5 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 module.exports = withBundleAnalyzer({
-  assetPrefix:"https://chatgptnextweb-kappa-one.vercel.app/"
+  assetPrefix:"https://langchain-nextjs-template-nu-sand.vercel.app/"
 })
